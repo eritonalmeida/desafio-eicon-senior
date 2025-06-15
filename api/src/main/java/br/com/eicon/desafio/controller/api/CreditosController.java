2b4f8910824720dd5f2ca9baae5feed028e77879
Eriton Almeida <eriton@mundilivre.com.br>
2025-06-15 00:54:28 -0300

    feat: api doc swagger

--- api/src/main/java/br/com/eicon/desafio/controller/api/CreditosController.java
+++ api/src/main/java/br/com/eicon/desafio/controller/api/CreditosController.java
@@ -4,10 +4,16 @@ import br.com.eicon.desafio.dto.CreditoDTO;
 import br.com.eicon.desafio.entity.Credito;
 import br.com.eicon.desafio.service.CreditoService;
 import br.com.eicon.desafio.service.CreditosKafkaPublisher;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/creditos")
+@Tag(name = "Créditos")
 public class CreditosController {
 
     @Autowired
@@ -24,6 +31,15 @@ public class CreditosController {
     @Autowired
     private CreditosKafkaPublisher creditosKafkaPublisher;
 
+    @Operation(
+            summary = "Créditos por numeroNfse",
+            responses = {
+                @ApiResponse(
+                        responseCode = "200",
+                        content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                schema = @Schema(implementation = CreditoDTO[].class)))
+            }
+    )
     @GetMapping("/{numeroNfse}")
     public ResponseEntity<List<CreditoDTO>> numeroNfse(@PathVariable("numeroNfse") String numeroNfse) {
 
@@ -40,6 +56,20 @@ public class CreditosController {
         return ResponseEntity.status(HttpStatus.OK).body(listDto);
     }
 
+    @Operation(
+            summary = "Créditos por numeroCredito",
+            responses = {
+                @ApiResponse(
+                        responseCode = "200",
+                        content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                schema = @Schema(implementation = CreditoDTO.class))),
+                @ApiResponse(
+                        responseCode = "204",
+                        content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                schema = @Schema(implementation = Void.class)))
+
+            }
+    )
     @GetMapping("/credito/{numeroCredito}")
     public ResponseEntity<CreditoDTO> numeroCredito(@PathVariable("numeroCredito") String numeroCredito) {
 
